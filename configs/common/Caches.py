f8aa7aa991be617ba21d241a2028fcba2d4a1f12
dwivedishivansh <157328252+dwivedishivansh@users.noreply.github.com>
2024-12-06 02:06:36 -0500

    Update Caches.py

--- configs/common/Caches.py
+++ configs/common/Caches.py
@@ -53,6 +53,7 @@ class L1Cache(Cache):
     response_latency = 2
     mshrs = 4
     tgts_per_mshr = 20
+    clusivity = "mostly_excl"
 
 class L1_ICache(L1Cache):
     is_read_only = True
@@ -70,6 +71,9 @@ class L2Cache(Cache):
     mshrs = 20
     tgts_per_mshr = 12
     write_buffers = 8
+    clusivity = "mostly_excl"
+    compressor = FPC()
+    tags = BaseSetAssoc()
 
 class IOCache(Cache):
     assoc = 8
